f1a672227ab626af42dee1d4b77b5454a1179776
Carlos <carlos.mur@gmail.com>
2020-11-29 21:59:24 -0300

    Update grava_scylla.py

--- grava_scylla.py
+++ grava_scylla.py
@@ -54,7 +54,7 @@ res = []
 for messagem in consumers.get_msg():
     texto = json.loads(messagem.value.decode('utf-8'))
     hashtg = "#".join(map(str, [r['text'] for r in texto['tweet']['entities']['hashtags']]))
-    
+
     dado.horario = str(texto['horario'])
     dado.created_at = str(texto['tweet']['created_at'])
     dado.desc_text = str(texto['tweet']['text']).rstrip().rstrip(os.linesep).replace('\n', '').\
